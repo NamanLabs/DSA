4577a4427e43871b052827870db97da1d3a9b7a8
Naman Kumar <namankumar281205@gmail.com>
2026-04-21 23:35:55 +0530

    Add files via upload

---
+++ array/basicsanddeclaration.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main() {
+    int arr[10] = {0,1,2,3,4,5,6,7,8,9};
+    // //input
+    // for (int i = 0; i < 10;i++) {
+    //     cin>>arr[i];
+    // }
+    //output
+    for(int i =0;i<=9;i++){
+        cout<<arr[i]<<" ";
+    }
+    // arr[0] =1;
+    // arr[1] =2;
+    // arr[2] =3;
+    // arr[3] =4;
+    // arr[4] =5;
+    // arr[5] =6;
+    // arr[6] =7;
+    // arr[7] =8;
+    // arr[8] =9;
+    // arr[9] =10;
+
+    return 0;
+}
