f21282d0ca3435cf4907d866f1cf0d471e9641aa
Naman Kumar <namankumar281205@gmail.com>
2026-04-27 13:35:16 +0530

    Add files via upload

---
+++ LAB 10/lcs.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int LCS(string X, string Y) {
+    int m = X.length(), n = Y.length();
+    vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+    for(int i = 1; i <= m; ++i) {
+        for(int j = 1; j <= n; ++j) {
+            if(X[i-1] == Y[j-1])
+                dp[i][j] = 1 + dp[i-1][j-1];
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+    return dp[m][n];
+}
+
+int main() {
+    string str1 = "AGGTAB", str2 = "GXTXAYB";
+    cout << "Length of Longest Common Subsequence: " << LCS(str1, str2) << endl;
+    return 0;
+}
